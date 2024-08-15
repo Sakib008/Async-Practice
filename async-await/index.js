a286bf47e5078cd38d5cb9547de88eae37e7abca
Sakib008 <mdshakib1902@gmail.com>
2024-08-15 20:24:52 +0530

    just practice

---
+++ async-await/index.js
@@ -0,0 +1,4 @@
+const output = document.querySelector('.output');
+const title = document.querySelector('.title');
+title.setAttribute('style', 'text-align  : center; font-size : 2rem; font-family : arial;font-weight : 700; background-color : #B51B75; padding : 1rem; margin : 2rem')
+title.style.color = '#E6FF94';
